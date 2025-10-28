7f32a9660331aac514fffe6f01db0741614d10db
Aron-RK <aronmarques9902@gmail.com>
2025-10-28 17:52:19 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// URL do backend no Render
 export const API = axios.create({
-  baseURL: "https://controle-compras-backend.onrender.com" //
+  baseURL: "https://controle-compras-backend.onrender.com", // sem /purchases
 });
