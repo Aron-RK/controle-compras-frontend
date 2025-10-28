1c365d73dd15c6e83c1e887ea705ba17b05bb61c
Aron-RK <aronmarques9902@gmail.com>
2025-10-28 17:26:44 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Substitua pela URL real do seu backend no Render
 export const API = axios.create({
-  baseURL: "https://SEU_BACKEND.onrender.com"
+  baseURL: "https://controle-compras-backend.onrender.com"
 });
