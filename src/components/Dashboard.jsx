ec6e9af0d3557772b8db14cf9eaca34e12f3a361
Aron-RK <aronmarques9902@gmail.com>
2025-10-28 17:21:04 -0300

    Create Dashboard.jsx

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+// src/components/Dashboard.jsx
+export default function Dashboard() {
+  return (
+    <div className="bg-white p-4 rounded-xl shadow-lg flex items-center justify-center h-32">
+      <p className="text-gray-500 font-semibold">Dashboard Placeholder</p>
+    </div>
+  );
+}
